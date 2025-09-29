80d70c3eef2bc70acae821dee33b68c4c38c2a06
wynsbssb <773921870@qq.com>
2025-09-30 07:43:45 +0800

    重构build.lua以使用固定的配置文件路径，并优化代码格式；新增rokit.toml以管理工具链

--- build/build.lua
+++ build/build.lua
@@ -1,11 +1,16 @@
 --// Libraries
+
 local fs = require("@lune/fs")
+
 local process = require("@lune/process")
+
 local serde = require("@lune/serde")
+
 local Base64 = require("../lib/reselim/base64@3.0.0/Base64")
 
 --// Configuration
-local ConfigFile = fs.readFile(process.args[1])
+local ConfigFile = fs.readFile("build/config.json")
+
 local Config = serde.decode("json", ConfigFile)
 
 local OutputFile = Config.output
@@ -16,56 +21,56 @@ local Frame = fs.readFile(Config.frame)
 local MainFile = fs.readFile(Config.main)
 
 local function GetTagPath(Tag: string, Path: string): string
-    local TagPath = Tags[Tag]
-    return Path:gsub(Tag, TagPath)
+	local TagPath = Tags[Tag]
+	return Path:gsub(Tag, TagPath)
 end
 
 --// Good enough
 local function GetPath(Path: string): string
-    local IsTag = Path:sub(1,1) == "@"
-    if IsTag then
-        local Tag = Path:split("/")[1]
-        Path = GetTagPath(Tag, Path)
-    end
+	local IsTag = Path:sub(1, 1) == "@"
+	if IsTag then
+		local Tag = Path:split("/")[1]
+		Path = GetTagPath(Tag, Path)
+	end
 
-    return Path
+	return Path
 end
 
 -- Format: --COMPILE: path
 local function ReplaceCompiles(Content: string): string
-    local Match = '[%-]*%s*"(%s*COMPILE:%s*@[^"]+)"'
+	local Match = '[%-]*%s*"(%s*COMPILE:%s*@[^"]+)"'
 
-    for String in Content:gmatch(Match) do
-        local PathReference = String:match('%s*COMPILE:%s*(@[^"]+)')
-        local Path = GetPath(PathReference)
+	for String in Content:gmatch(Match) do
+		local PathReference = String:match('%s*COMPILE:%s*(@[^"]+)')
+		local Path = GetPath(PathReference)
 
-        local Contents = fs.readFile(Path)
-        local ContentBuffer = buffer.fromstring(Contents)
-        local Replacement = Base64:Encode(ContentBuffer)
+		local Contents = fs.readFile(Path)
+		local ContentBuffer = buffer.fromstring(Contents)
+		local Replacement = Base64:Encode(ContentBuffer)
 
-        Content = Content:gsub(String, function()
-            return buffer.tostring(Replacement)
-        end)
-    end
+		Content = Content:gsub(String, function()
+			return buffer.tostring(Replacement)
+		end)
+	end
 
-    return Content
+	return Content
 end
 
 -- Format: --INSERT: path
 local function ReplaceInserts(Content: string): string
-    local Match = "(%-%-%s*INSERT:%s*@?[^%s]+)"
+	local Match = "(%-%-%s*INSERT:%s*@?[^%s]+)"
 
-    for String in Content:gmatch(Match) do
-        local PathReference = String:match('%s*INSERT:%s*(@[^"]+)')
-        local Path = GetPath(PathReference)
+	for String in Content:gmatch(Match) do
+		local PathReference = String:match('%s*INSERT:%s*(@[^"]+)')
+		local Path = GetPath(PathReference)
 
-        local Contents = fs.readFile(Path)
-        Content = Content:gsub(String, function()
-            return Contents
-        end)
-    end
+		local Contents = fs.readFile(Path)
+		Content = Content:gsub(String, function()
+			return Contents
+		end)
+	end
 
-    return Content
+	return Content
 end
 
 --// Compile
