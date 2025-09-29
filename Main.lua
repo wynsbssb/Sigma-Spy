384b14ad1ce53c4c7af3d9f0ecdf2ec450b30afa
wynsbssb <773921870@qq.com>
2025-09-30 07:47:06 +0800

    在Main.lua中添加中文标记以增强可读性

--- Main.lua
+++ Main.lua
@@ -15,7 +15,7 @@
 	@repo https://github.com/depthso
 	@license MIT
 	@description Sigma Remote Spy for Roblox
-    
+    @wynsbssb Chinese
 	This file is COMPILED, check /src folder for the source
 	Build scripts are available in /build
 ]]
